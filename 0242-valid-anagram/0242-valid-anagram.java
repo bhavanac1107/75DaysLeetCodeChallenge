63a8850ada8c6bafa7f7bb38cc74fe0388395706
Bhavana.C <bhavanac1107@gmail.com>
2026-03-14 11:49:32 +0530

    Time: 2 ms (99.56%), Space: 44.4 MB (72.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,23 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
+         if (s.length() != t.length()) return false;
 
         int[] count = new int[26];
 
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
         }
 
-        for (int c : count) {
-            if (c != 0) {
-                return false;
-            }
+        for (char c : t.toCharArray()) {
+            if (--count[c - 'a'] < 0) return false;
         }
 
         return true;
-
     }
 }
